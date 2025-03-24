c8b0eb00663d7a369e6448ee3b0f149f2d035698
niwwka <senya123op@yandex.ru>
2025-03-24 22:23:14 +0300

    create window

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 pygame.init()
 
-window_height = 500
-window_width = 600
+window_height = 550
+window_width = 700
 window  = pygame.display.set_mode((window_height,window_width))
+
+while True:
+    pass
