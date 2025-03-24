4b14e8b0c3413d1c1786e2a85428772aa558c485
niwwka <senya123op@yandex.ru>
2025-03-24 22:40:58 +0300

    customize something

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import pygame
 pygame.init()
 
+# характеристики
+pygame.display.set_caption('Calculator')
 window_height = 550
 window_width = 700
-window  = pygame.display.set_mode((window_height,window_width))
+window = pygame.display.set_mode((window_height, window_width))
+color = (75, 0, 130)
+window.fill(color)
+pygame.display.flip()
 
-while True:
-    pass
+# bg = pygame.image.load("calc.jpg")
+
+
+
+# код
+run = True
+while run:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            run = False
