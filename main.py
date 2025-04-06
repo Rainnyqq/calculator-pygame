aabfca4bdf97d9da9ab12cd95e66f33451312df5
niwwka <senya123op@yandex.ru>
2025-04-06 20:19:52 +0300

    добавили кнопку

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import utils
 
 
 root = utils.window_setup()
-
-
 label = Label(text="Привет!")
 label.pack()
 
 
 
-
-
+def click_button():
+    print(1)
+btn = ttk.Button(text="Click Me", command=click_button)
+btn.pack()
 
 
 
