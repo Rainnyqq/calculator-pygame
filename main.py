245e92ce60d50bd96b2ce6f13446b9b6e888f089
niwwka <senya123op@yandex.ru>
2025-04-06 20:45:33 +0300

    добавили 1

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ label.pack()
 
 
 
-def click_button():
+def button_1():
     print(1)
-btn = ttk.Button(text="Click Me", command=click_button)
-btn.pack()
+btn = ttk.Button(text="1", command=button_1())
+btn.place(relx = 0,rely = 0.9, relheight=0.1, relwidth=1/3)
+
+# def button_2():
+#     print(2)
+# btn = ttk.Button(text="2", command=button_2())
+# btn.pack(anchor=S, expand=True, ipadx=10, ipady=15)
 
 
 
